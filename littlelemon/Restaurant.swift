e2c5892d161bf030a03bf6d32c5e6b855dba0920
emonte9 <montero.edgar1997@gmail.com>
2024-11-12 22:38:18 -0600

    hi

---
+++ littlelemon/Restaurant.swift
@@ -0,0 +1,18 @@
+//
+//  Restaurant.swift
+//  littlelemon
+//
+//  Created by Edgar Montero on 11/12/24.
+//
+
+import SwiftUI
+
+struct Restaurant: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Restaurant()
+}

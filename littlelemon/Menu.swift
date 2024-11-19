b01d2dca399ac9140c5f4b701214dbfc68dc5440
emonte9 <montero.edgar1997@gmail.com>
2024-11-19 02:14:44 -0600

    "finish user boarding and user profile"

---
+++ littlelemon/Menu.swift
@@ -0,0 +1,18 @@
+//
+//  Menu.swift
+//  littlelemon
+//
+//  Created by Edgar Montero on 11/18/24.
+//
+
+import SwiftUI
+
+struct Menu: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Menu()
+}
---
+++ littlelemon/UserProfile.swift
@@ -0,0 +1,18 @@
+//
+//  UserProfile.swift
+//  littlelemon
+//
+//  Created by Edgar Montero on 11/18/24.
+//
+
+import SwiftUI
+
+struct UserProfile: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UserProfile()
+}
